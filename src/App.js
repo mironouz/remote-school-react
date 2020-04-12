be0518ffb17c2c44afc6af2e781c646708d0a84e
Dmitrii Mironov <mironouz@gmail.com>
2020-04-12 06:15:58 +0200

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const [users, setUsers] = useState([])
+
+
+const onSubmit = e => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  var object = {};
+  data.forEach((value, key) => {object[key] = value});
+  fetch('/api/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(object)
+  });
 }
 
-export default App;
+useEffect(() => {
+  let eventSource = new EventSource('http://localhost:8080/api/users')
+    eventSource.onmessage = e => {
+        const parsedData = JSON.parse(e.data);
+        setUsers((user) => users.concat(parsedData));
+    }}, [users]);
+
+
+  return (
+    <form onSubmit={onSubmit} action="/api/register" method="post">
+      <label for="name">Имя</label>
+      <input type="text" name="name"/>
+      <br/>
+      <label for="surname">Фамилия</label>
+      <input type="text" name="surname"/>
+      <br/>
+      <br/>
+      <label for="grade">Класс</label>
+      <input type="text" name="grade"/>
+      <button>Отправить</button>
+    {users.map((user) => <p>{user.name}  {user.surname}  {user.grade}</p>)}
+    </form>
+  );
+}
+export default App
