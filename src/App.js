04d7e90434a09f4cd218dd201d297a096b39f404
Dmitrii Mironov <mironouz@gmail.com>
2020-05-03 04:05:19 +0200

    use messages endpoint

--- src/App.js
+++ src/App.js
@@ -3,26 +3,52 @@ import './App.css';
 import 'eventsource/example/eventsource-polyfill'
 
 export default function App() {
-    const [users, setUsers] = useState([])
+    const [messages, setMessages] = useState([])
 
     useEffect(() => {
         let user = JSON.parse(localStorage.getItem('user'))
-        let eventSource = new window.EventSourcePolyfill('/api/users', {
+        let userMessages = new window.EventSourcePolyfill('/api/messages', {
             headers: {
                 'Authorization': 'Basic ' + user.auth
             }})
-        eventSource.onmessage = e => {
+        userMessages.onmessage = e => {
             const parsedData = JSON.parse(e.data);
-            setUsers(users => [parsedData, ...users]);
+            setMessages(messages => [parsedData, ...messages]);
         }
+        userMessages.onopen = _ => setMessages([])
     }, []);
 
     return (
         <div className="Wrapper">
-            <p>Messages:</p>
+            <form className="MessageForm" onSubmit={sendMessage}>
+                <label htmlFor="text">Текст сообщения</label><br/>
+                <input type="text" name="text" id="text"/><br/>
+                <button>Отправить</button>
+            </form>
             <div className="UserList">
-                {users.map((user, i) => <p key={i}>{user.name} {user.surname} {user.grade}</p>)}
+                {messages.map((m, i) =>
+                    <p key={i}>{m.user.name} {m.user.surname} : {m.text} {m.timestamp}</p>)}
             </div>
         </div>
     );
+}
+
+const sendMessage = e => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    let user = JSON.parse(localStorage.getItem('user'))
+    let message = {};
+    message.user = {'name': user.name, 'surname': user.surname, 'grade': user.grade}
+    message.timestamp = Date.now()
+    data.forEach((value, key) => {
+        message[key] = value
+    });
+    fetch('/api/message', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic ' + user.auth
+        },
+        body: JSON.stringify(message)
+    }).then()
 }
